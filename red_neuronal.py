8077a7d1a04478b9c80ad9ba18d7e0e1d573d1c2
JEHYP1021 <jehypo1021@gmail.com>
2025-04-05 22:54:09 +0200

    version 1.0.0

---
+++ red_neuronal.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+celsios = np.array([-40, -10, 0, 8, 15, 22, 38], dtype=float)
+fahrenheit = np.array([-40, 14, 32, 46, 59, 72, 100], dtype=float)
+
+modelo = tf.keras.Sequential([tf.keras.Input(shape=(1,)), tf.keras.layers.Dense(units=1)])
+
+modelo.compile(optimizer = tf.keras.optimizers.Adam(0.1), loss = 'mean_squared_error')
+
+print("Comenzando entrenamiento...")
+historial = modelo.fit(celsios, fahrenheit, epochs=1000, verbose=False)
+print("Modelo entrenado!")
+
+plt.xlabel("#Epoca")
+plt.ylabel("Magnitud de perdida")
+plt.plot(historial.history["loss"])
+plt.show()
