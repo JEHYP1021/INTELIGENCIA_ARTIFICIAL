036c15bffa38ef13de482ac4fdc62ed90c54463c
JEHYP1021 <jehypo1021@gmail.com>
2025-04-06 15:56:19 +0200

    version 1.0.2

--- red_neuronal.py
+++ red_neuronal.py
@@ -8,12 +8,16 @@ fahrenheit = np.array([-40, 14, 32, 46, 59, 72, 100], dtype=float)
 modelo = tf.keras.Sequential([tf.keras.Input(shape=(1,)), tf.keras.layers.Dense(units=1)])
 
 modelo.compile(optimizer = tf.keras.optimizers.Adam(0.1), loss = 'mean_squared_error')
-
-print("Comenzando entrenamiento...")
-historial = modelo.fit(celsios, fahrenheit, epochs=1000, verbose=False)
-print("Modelo entrenado!")
+if __name__=="__main__":
+    print("Comenzando entrenamiento...")
+    historial = modelo.fit(celsios, fahrenheit, epochs=1000, verbose=False)
+    print("Modelo entrenado!")
 
 plt.xlabel("#Epoca")
 plt.ylabel("Magnitud de perdida")
 plt.plot(historial.history["loss"])
 plt.show()
+
+print("Hagamos una predicción:")
+resultado = modelo.predict(np.array([100.0]))
+print(f'El resultado es: {resultado[0][0]:.2f} fahrenheit')
